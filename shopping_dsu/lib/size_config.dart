1d38fc6052f98788a1f74b810cc59276a9052cea
Kimyeongseo-github <80802859+Kimyeongseo-github@users.noreply.github.com>
2023-11-13 18:51:49 +0900

    Update size_config.dart

--- shopping_dsu/lib/size_config.dart
+++ shopping_dsu/lib/size_config.dart
@@ -15,14 +15,12 @@ class SizeConfig {
   }
 }
 
-// Get the proportionate height as per screen size
+// 화면 크기에 비례
 double getProportionateScreenHeight(double inputHeight) {
   double screenHeight = SizeConfig.screenHeight;
   // 812 is the layout height that designer use
   return (inputHeight / 812.0) * screenHeight;
 }
-
-// Get the proportionate height as per screen size
 double getProportionateScreenWidth(double inputWidth) {
   double screenWidth = SizeConfig.screenWidth;
   // 375 is the layout width that designer use
