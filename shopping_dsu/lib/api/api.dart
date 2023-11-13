215365ac280fefb9a62047905c95438813bef37a
Kimyeongseo-github <80802859+Kimyeongseo-github@users.noreply.github.com>
2023-11-13 19:18:48 +0900

    졸업작품

--- shopping_dsu/lib/api/api.dart
+++ shopping_dsu/lib/api/api.dart
@@ -1,5 +1,5 @@
 class API {
-  static const hostConnect = "http://192.168.123.100/vege_php";
+  static const hostConnect = "http://ip주소/vege_php";
   // 본인의 ipv4 주소 입력
   static const hostConnectUser = "$hostConnect/user";
   static const signup = "$hostConnect/user/signup.php";
